cf555ed06718abb9a70e19989ef833f49d7ef946
Omid Reza Heidari <omid.orh@gmail.com>
2024-11-27 12:23:39 -0500

    Make structured

--- Project/Analyze.py
+++ Project/Analyze.py
@@ -1,10 +1,23 @@
-from Project.preprocess import preprocess
 import seaborn as sns
 import matplotlib.pyplot as plt
+from Project.preprocess import preprocess, convert_llm_cols_to_row
 
 data = preprocess('Result.xlsx')
 for llm in ['GPT 4-o', 'Claud 3.5', 'Gemini 1.5']:
     ax = sns.barplot(data, x="local_index", y=llm, hue="National")
     ax.set(xlabel='Questions', ylabel='Responses')
-    ax.figure.savefig("{}.png".format(llm),dpi=600)
+    ax.figure.savefig("output/{}.png".format(llm),dpi=600)
     plt.clf()
+
+data = convert_llm_cols_to_row(data)
+
+
+ax = sns.catplot(data, x="local_index", y='LLM Response', hue="National", col="LLM Name", kind="bar")
+ax.set(xlabel='Questions', ylabel='Responses')
+ax.figure.savefig("output/LLMs.png",dpi=600)
+plt.clf()
+
+ax = sns.catplot(data, x="National", y='LLM Response', hue="LLM Name", col="local_index", kind="bar")
+ax.set(xlabel='Questions', ylabel='Responses')
+ax.figure.savefig("output/Questions.png",dpi=600)
+plt.clf()
--- Project/preprocess.py
+++ Project/preprocess.py
@@ -13,8 +13,10 @@ def preprocess(file_path: str):
             tmp_nums = list(map(int, data.at[data.index[cl_num], llm].split(',')))
             data.at[data.index[cl_num], llm] = (tmp_nums[0] - 1) * 4 + tmp_nums[1]
     data = data.drop([9, 19, 29, 39])
-    # del data['Question']
     data["GPT 4-o"] = pd.to_numeric(data["GPT 4-o"])
     data["Claud 3.5"] = pd.to_numeric(data["Claud 3.5"])
     data["Gemini 1.5"] = pd.to_numeric(data["Gemini 1.5"])
     return data
+
+def convert_llm_cols_to_row(data):
+    return pd.melt(data, id_vars=['Question', 'National', 'local_index'], value_vars=['GPT 4-o', 'Claud 3.5', 'Gemini 1.5'], var_name='LLM Name', value_name='LLM Response')
