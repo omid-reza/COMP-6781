9934549da1c7a4be29ec2ec49ab8c5f0c8e5530d
Omid Reza Heidari <omid.orh@gmail.com>
2024-12-08 09:46:32 -0500

    config 50epc

--- Assignment3/Main.sh
+++ Assignment3/Main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -J 20-6781
+#SBATCH -J 50-6781
 #SBATCH --mem=85GB
 #SBATCH --gpus=1
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=omid.orh@gmail.com
 #SBATCH --partition=phys
-#SBATCH -o COMP6781-A3-20epoch.out
+#SBATCH -o COMP6781-A3-50epoch.out
 #SBATCH -w virya2
 
 module load anaconda
